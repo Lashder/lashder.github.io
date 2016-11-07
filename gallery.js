edb694a5962b82c0dfe370e288df30be2735382e
Lashder <lashder@gmail.com>
2016-11-06 23:54:55 -0800

    Adding simple image gallery

---
+++ gallery.js
@@ -0,0 +1,57 @@
+var SCROLL_AMOUNT_PX = 1;
+var SCROLL_INTERVAL_MS = 20;
+
+var appendGallery = function() {
+  var gallery = $('<div class="gallery"/>');
+  manifest.forEach(function(imageSet) {
+    imageSet.forEach(function(image) {
+      var imageUrl = 'images/' + image;
+      var item = $('<div class="item"/>');
+      item.append('<img src="' + imageUrl + '"/>');
+      gallery.append(item);
+    });
+  });
+  gallery.css({
+    left: $('.gallery').length > 0 ? $('#gallery-container').outerWidth() : 0
+  });
+  $('#gallery-container').append(gallery);
+};
+
+var doScroll = function() {
+  $('.gallery').css({
+    left: '-=' + SCROLL_AMOUNT_PX
+  });
+  var leftMost = $($('.gallery:not(.old)')[0]);
+  var width = parseInt(leftMost.outerWidth());
+  var left = parseInt(leftMost.offset().left);
+  var right = left + width;
+  if (right <= parseInt($(window).width())) {
+    appendGallery();
+    leftMost.addClass('old');
+  }
+  $('.gallery.old').each(function(el) {
+    var width = parseInt($(this).outerWidth());
+    var left = parseInt($(this).offset().left);
+    if (left <= (-1 * width)) {
+      $(this).remove();
+    }
+  });
+};
+
+var galleryOnLoad = function(manifest) {
+  window.manifest = manifest;
+  appendGallery();
+  var scrollInterval = setInterval(doScroll, SCROLL_INTERVAL_MS);
+  $(window).focus(function() {
+    if (scrollInterval) {
+      clearInterval(scrollInterval);
+    }
+    scrollInterval = setInterval(doScroll, SCROLL_INTERVAL_MS);
+  }).blur(function() {
+    clearInterval(scrollInterval);
+  });
+};
+
+var manifestScript = document.createElement('script');
+manifestScript.src = 'images/manifest.js';
+document.body.appendChild(manifestScript);
